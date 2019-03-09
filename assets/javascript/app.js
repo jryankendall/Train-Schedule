0a55063ec712302ded8d908c3c2833edc5a96045
unknown <jed@jedkendall.com>
2019-03-08 21:41:01 -0600

    Basic functionality to add to firebase added. Page automatically prints data from the firebase into the table too.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,15 @@
 $( function() {
     $(document.body).on("click", ".add-train-button", function(event) {
         event.preventDefault();
+        
+        
+        $(".half-size").removeClass("red-warning-text");
         addTrain();
-    })
-})
+        if (validTrainRow) {
+            storeTrain();
+        } else
+        {
+            console.log("Invalid entry. Check the boxes.");
+        };
+    });
+});
--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -5,39 +5,99 @@ var database = firebase.database();
 var trainName = "";
 var trainDestination = "";
 var firstTrainTime = "";
-var trainFrequency = "";
+var trainFrequency = 0;
+var validTrainRow;
+
+var lastTrainIndex = 1;
 
 //
 var invalidNums = /^[0-9]^\072/gi;
-var validNums = /[0-9]^[a-z]/gi;
+var validNums = /[0-9]/gi;
+var alphaNumerics = /[a-z]/gi;
 
 function addTrain() {
+
     var validName = false;
     var validDest = false;
     var validTime = false;
     var validFreq = false;
-    if ($("#input-train-name").val().length() < 21) {
+
+    if ($("#input-train-name").val().length < 21 && $("#input-train-name").val().trim().length > 0 && alphaNumerics.test($("#input-train-name").val().trim()) == true) {
         trainName = $("#input-train-name").val().trim();
         validName = true;
-    }
-    if ($("#input-train-destination").val().length() < 21) {
+    };
+    if ($("#input-train-destination").val().length < 21 && $("#input-train-destination").val().trim().length > 0 && alphaNumerics.test($("#input-train-destination").val().trim()) == true) {
         trainDestination = $("#input-train-destination").val().trim();
         validDest = true;
-    }
-    if ($("#input-train-time").val().length() == 5 && $("#input-train-time").val().test(invalidNums) == false) {
+    };
+    if ($("#input-train-time").val().length == 5 && invalidNums.test($("#input-train-time").val()) == false) {
         firstTrainTime = $("#input-train-time").val().trim();
         validTime = true;
-    }
-    if ($("#input-train-frequency").val().length <= 3 && $("#input-train-time").val().test(validNums) == true && 
-        parseInt($("#input-train-frequency").val()) > 0) {
-            validFreq = true;
-    }
+    };
+    if ($("#input-train-frequency").val().length <= 3 && validNums.test($("#input-train-frequency").val()) == true && parseInt($("#input-train-frequency").val(), 10) > 0) {
+        trainFrequency = parseInt($("#input-train-frequency").val(), 10);
+        validFreq = true;
+    };
 
     if (validFreq == false || validName == false || validDest == false || validTime == false) {
-        var validsArray = [];
-        validsArray.push(validName)
-                    .push(validDest)
-                    .push(validTime)
-                    .push(validFreq);
-    }
+        if (!validName) {
+            $("#name-warning").addClass("red-warning-text");
+        };
+        if (!validDest) {
+            $("#destination-warning").addClass("red-warning-text");
+        };
+        if (!validTime) {
+            $("#time-warning").addClass("red-warning-text");
+        };
+        if (!validFreq) {
+            $("#frequency-warning").addClass("red-warning-text");
+        };
+
+        
+        validTrainRow = false;
+    } else
+    {   
+        validTrainRow = true;
+    };
+};
+
+function storeTrain() {
+    database.ref("trains/").push( {
+        tName : trainName,
+        tDest : trainDestination,
+        tFirstTime : firstTrainTime,
+        tFrequency : trainFrequency,
+        dateAdded : firebase.database.ServerValue.TIMESTAMP
+    })
 }
+
+//
+
+function printTrains() {
+
+};
+
+database.ref("trains/").on("child_added", function(valueSnap){
+    var listTName = "<td>" + valueSnap.val().tName + "</td>";
+    var listTDest = "<td>" + valueSnap.val().tDest + "</td>";
+    var listTFirstTime = "<td>" + valueSnap.val().tFirstTime + "</td>";
+    var listTFrequency = "<td>" +  valueSnap.val().tFrequency + "</td>";
+
+    var newRow = $("<tr>");
+    var newTHead = $("<th>");
+    newTHead.attr("scope","row");
+    newTHead.text(lastTrainIndex);
+
+    newRow.append(newTHead)
+        .append(listTName)
+        .append(listTDest)
+        .append(listTFirstTime)
+        .append(listTFrequency)
+        .append("<td>69</td>");
+
+    $("#train-table-body").append(newRow);
+    
+    lastTrainIndex++;
+}, function(errorObject) {
+    console.log(errorObject.code);
+})
