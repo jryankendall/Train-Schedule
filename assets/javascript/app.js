7f83e68bcb296b9adb38f5a48a487a42bc782f06
unknown <jed@jedkendall.com>
2019-03-11 15:57:10 -0500

    Finished, regexp issues should be resolved. Hopefully works as intended.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,25 @@
 $( function() {
     $(document.body).on("click", ".add-train-button", function(event) {
         event.preventDefault();
+        trainName = "";
+        trainDestination = "";
+        firstTrainTime = "";
+        trainFrequency = 0;
+        validTrainRow = false;
         
-        
+        //Clears the red warning labels from the page
         $(".half-size").removeClass("red-warning-text");
         addTrain();
-        if (validTrainRow) {
-            storeTrain();
-        } else
-        {
-            console.log("Invalid entry. Check the boxes.");
-        };
+
+        //Makes sure the input checker runs before this function
+        setTimeout(function() {
+            if (validTrainRow) {
+                storeTrain();
+            } else
+            {
+                console.log("Invalid entry. Check the boxes.");
+            };
+        }, 250);
+        
     });
 });
--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -8,31 +8,36 @@ var firstTrainTime = "";
 var trainFrequency = 0;
 var validTrainRow;
 
+//This var is used to append numbers to the left side of the table in order
 var lastTrainIndex = 1;
 
 //
 
 function addTrain() {
 
-    var invalidNums = /^[0-9]^\072/gi;
-    var validNums = /[0-9]/gi;
-    var alphaNumerics = /[a-z]/gi;
+    var invalidNums = /^[0-9]^\072/gi; //Checked against time to make sure it's only made up of numbers and a :
+    var validNums = /[0-9]/gi; //Checked against frequency input to make sure it's an integer
+    var alphaNumerics = /^[a-z0-9]^\s/gi; //Checked against name/destination to make sure it's only letters, numbers, and spaces
 
     var validName = false;
     var validDest = false;
     var validTime = false;
     var validFreq = false;
 
-    if ($("#input-train-name").val().length < 21 && $("#input-train-name").val().trim().length > 0 && alphaNumerics.test($("#input-train-name").val().toString().trim()) === true) {
+    //Makes sure train name is correct length and characters
+    if ($("#input-train-name").val().length < 21 && $("#input-train-name").val().trim().length > 0 && alphaNumerics.test($("#input-train-name").val().toString().trim()) === false) {
         trainName = $("#input-train-name").val().trim();
         validName = true;
 /*         console.log("Name Checked"); */
     };
-    if ($("#input-train-destination").val().length < 21 && $("#input-train-destination").val().trim().length > 0 && alphaNumerics.test($("#input-train-destination").val().toString().trim()) === true) {
+    //Makes sure destination is correct length and characters
+    if ($("#input-train-destination").val().length < 21 && $("#input-train-destination").val().trim().length > 0 && alphaNumerics.test($("#input-train-destination").val().toString().trim()) === false) {
         trainDestination = $("#input-train-destination").val().trim();
         validDest = true;
 /*         console.log("Destination Checked"); */
     };
+    //Checks input arrival time against several factors. Makes sure it is 5 characters long
+    //Makes sure the first two numbers are between 00 and 24 inclusive, and the second two numbers are between 00 and 59 inclusive
     if ($("#input-train-time").val().length == 5 && invalidNums.test($("#input-train-time").val().toString()) === false) {
         var timeArray = $("#input-train-time").val().split(":");
         if (timeArray[0] <= 24 && timeArray[0] >= 00) {
@@ -43,12 +48,14 @@ function addTrain() {
         }
 /*         console.log("Time Checked"); */
     };
+    //Makes sure the frequency is a number between 0 and 999 and not a negative number
     if ($("#input-train-frequency").val().length <= 3 && validNums.test($("#input-train-frequency").val().toString()) === true && parseInt($("#input-train-frequency").val(), 10) > 0) {
         trainFrequency = parseInt($("#input-train-frequency").val(), 10);
         validFreq = true;
 /*         console.log("Frequency Checked"); */
     };
 
+    //simply makes sure this doesnt check the booleans until after the if statements above
     setTimeout(function() {
         
         if (validFreq == false || validName == false || validDest == false || validTime == false) {
@@ -68,15 +75,16 @@ function addTrain() {
 /*             console.log("" + validName + validDest + validTime + validFreq); */
 
             
-            validTrainRow = false;
+            validTrainRow = false; //Refuses user input train
         } else
         {   
-            validTrainRow = true;
+            validTrainRow = true; //Allows user input train to be appended
         };
-    }, 100);
+    }, 0);
 
 };
 
+//Database function that stores the user's input train in the database
 function storeTrain() {
     database.ref("trains/").push( {
         tName : trainName,
@@ -85,15 +93,17 @@ function storeTrain() {
         tFrequency : trainFrequency,
         dateAdded : firebase.database.ServerValue.TIMESTAMP
     })
+    //Empties all the fields after pressing submit
     $(".train-form-input").val("");
 }
 
 //
 
-function printTrains() {
+/* function printTrains() {
+    Does nothing, turned out to be an unnecessary function. Left in for posterity.
+}; */
 
-};
-
+//Function that prints out the database info on the page, tracks any changes to it.
 database.ref("trains/").on("child_added", function(valueSnap){
     var timeNow = moment();
 
@@ -130,6 +140,7 @@ database.ref("trains/").on("child_added", function(valueSnap){
     var listNextTime = "<td>" + nextTrainMin + "</td>";
     listTFirstTime = "<td>" + firstTimeMoment.format("HH:mm") + "</td>";
 
+    //Prints out the new row
     newRow.append(newTHead)
         .append(listTName)
         .append(listTDest)
